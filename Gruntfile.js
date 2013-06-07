412fdb2ac955a59a9978ae61f9707c85c8b349dc
Juan Pablo Kaniefsky <jpkaniefsky@gmail.com>
2013-06-07 18:41:56 -0300

    Inflexion point.

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    concat: {
+      build: {
+        src: ["components/jpka-sl/index.html", "component.html"],
+        dest: "index.html"
+      }
+    },
+    watch: {
+      build: {
+        files: "<%= concat.build.src %>",
+        tasks: ["build"]
+      }
+    }
+  });
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+  grunt.registerTask("karma", function() {
+    require("child_process").spawn("node_modules/.bin/karma", ["start"], {stdio: "inherit"})
+      .on("exit", function() {
+      });
+  });
+
+  grunt.registerTask("build", ["concat:build"]);
+  grunt.registerTask("test", ["build", "karma", "watch:build"]);
+}
--- test/specs/spec.js
+++ test/specs/spec.js
@@ -1,7 +1,61 @@
-describe("my-element", function() {  
-  var element;
+describe("socket.io-sl", function() {
+  var element,
+  stubSocket = {
+    listeners: {},
+    on: function(what, fn) {
+      var listeners = this.listeners;
+      if (!listeners[what]) {
+        listeners[what] = [];
+      }
+      listeners[what].push(fn);
+    },
+    emit: function(what, arg) {
+      var listeners = this.listeners[what];
+      if (listeners) {
+        listeners.forEach(function(fn) {
+          fn(arg);
+        });
+      }
+    }
+  },
+  stubIO = {
+    connect: function() {
+      return stubSocket;
+    }
+  };
+
+  function populateList() {
+    element.add("element-with-model", [{id: "a"}, {id: "b"}]);
+  }
 
   beforeEach(function() {
-    element = fixtures.window().document.createElement("my-element");
+    element = fixtures.window().document.createElement("jpka-socket.io-sl");
+    element.childTagName = "element-with-model";
+    element.path = "/path";
+    element.io = stubIO;
   });
+
+  it("should respond to create events", function() {
+    var model = {id: "a", a: 1, b: 2};
+    stubSocket.emit("create", model);
+    expect(element.childNodes.length).to.equal(1);
+    expect(element.childNodes[0].model).to.deep.equal(model);
+    expect(element.childNodes[0].id).to.equal("a");
+  });
+
+  /*it("should respond to delete events", function() {
+    populateList();
+    stubSocket.emit("delete", {id: "1"});
+
+    expect(element.childNodes.length).to.equal(1);
+    expect(element.childNodes[0].id).to.equal("2");
+  });*/
+
+  /*it("should respond to update events", function() {
+    var model = {id: "b", a: 1, b: 2};
+    populateList();
+    stubSocket.emit("update", model);
+
+    expect(element.querySelector("#b").model).to.deep.equal(model);
+  });*/
 });
