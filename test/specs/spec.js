2c18f66f060d49ddf64eeba601d108d8db4a2cfc
Juan Pablo Kaniefsky <jpkaniefsky@gmail.com>
2013-06-16 16:50:38 -0300

    Added reverse mode

--- test/specs/spec.js
+++ test/specs/spec.js
@@ -1,6 +1,6 @@
 describe("racer-list", function() {
   var element,
-  coll,
+  list,
   modelData = {
     items: [
       {a: 1},
@@ -12,26 +12,38 @@ describe("racer-list", function() {
   beforeEach(function(done) {
     Model = fixtures.window().Model;
     element = fixtures.window().document.querySelector("racer-list").cloneNode();
-    coll = element.collection;
+    list = element.list;
     element.on("model:load", function() {
       done();
     });
     model = element.model = new Model({items: []});
   });
 
-  it("should have a collection", function() {
-    expect(coll).to.exist;
-    expect(coll.nodeName).to.equal("SPAN");
+  it("should have a list", function() {
+    expect(list).to.exist;
+    expect(list.nodeName).to.equal("SPAN");
   });
 
-  it("should populate the collection when a model is attached", function(done) {
+  it("should populate the list when a model is attached", function(done) {
     var model = new Model(modelData);
 
     element.on("model:load", function() {
-      expect(coll.items.length).to.equal(2);
+      expect(list.items.length).to.equal(2);
       modelData.items.forEach(function(data, i) {
-        expect(coll.items[i].child.model).to.equal(data);
+        expect(list.items[i].child.model).to.equal(data);
       });
+      done();
+    });
+    element.model = model;
+  });
+
+  it("should populate on reverse if reverse is declared", function(done) {
+    var model = new Model(modelData);
+    element.reverse = true;
+
+    element.on("model:load", function() {
+      expect(list.items[0].child.model).to.equal(modelData.items[1]);
+      expect(list.items[1].child.model).to.equal(modelData.items[0]);
       done();
     });
     element.model = model;
@@ -49,27 +61,20 @@ describe("racer-list", function() {
       element.model.data.items.push(modelData.items[1]);
       model.emit("items", "insert", 2, modelData.items[1]);
 
-      expect(coll.items.length).to.equal(3);
-      wrapper = coll.items[2];
+      expect(list.items.length).to.equal(3);
+      wrapper = list.items[2];
       expect(wrapper.nodeName).to.equal("RACER-ELEMENT");
       expect(wrapper.child.nodeName).to.equal("ELEMENT-WITH-MODEL");
       expect(wrapper.child.model).to.deep.equal(modelData.items[1]);
     });
 
-    it("should manage when a document is prepended", function() {
-      element.model.data.items.unshift(modelData.items[1]);
-      model.emit("items", "insert", 0, modelData.items[1]);
+    it("should manage when a document is appended in reverse mode", function() {
+      element.reverse = true;
+      element.model.data.items.push(modelData.items[1]);
+      model.emit("items", "insert", 2, modelData.items[1]);
 
-      expect(coll.items.length).to.equal(3);
-      expect(coll.items[0].child.model).to.deep.equal(modelData.items[1]);
-    });
-
-    it("should manage when a document is inserted at a random index", function() {
-      element.model.data.items = [modelData.items[0], modelData.items[1], modelData.items[0]];
-      model.emit("items", "insert", 1, modelData.items[1]);
-
-      expect(coll.items.length).to.equal(3);
-      expect(coll.items[1].child.model).to.deep.equal(modelData.items[1]);
+      expect(list.items.length).to.equal(3);
+      expect(list.items[0].child.model).to.deep.equal(modelData.items[1]);
     });
   });
 
@@ -77,14 +82,15 @@ describe("racer-list", function() {
     element.model.data.items.push(modelData.items[0]);
     element.push(modelData.items[0]);
     model.emit("items.0", "change", modelData.items[1]);
-    expect(coll.childNodes[0].child.model).to.deep.equal(modelData.items[1]);
+    expect(list.childNodes[0].child.model).to.deep.equal(modelData.items[1]);
   });
 
   it("should delete when the document is deleted", function() {
     element.push(modelData.items[0]);
     element.push(modelData.items[1]);
     model.emit("items", "remove", 1);
-    expect(coll.items.length).to.equal(1);
-    expect(coll.items[0].child.model).to.equal(modelData.items[0]);
+    expect(list.items.length).to.equal(1);
+    expect(list.items[0].child.model).to.equal(modelData.items[0]);
   });
+
 });
